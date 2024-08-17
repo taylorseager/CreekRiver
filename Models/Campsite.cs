ad787b07fd80a9ba896465330cedb0679ba2cf14
Taylor Seager <taylorseager718@gmail.com>
2024-08-17 12:15:31 -0500

    save point

--- Models/Campsite.cs
+++ Models/Campsite.cs
@@ -7,8 +7,8 @@ public class Campsite
     public int Id { get; set; }
     [Required]
     public required string Nickname { get; set; }
-    public string? ImageUrl { get; set; }
-    public int? CampsiteTypeId { get; set; }
+    public string ImageUrl { get; set; }
+    public int CampsiteTypeId { get; set; }
     public CampsiteType? CampsiteType { get; set; }
     public List<Reservation>? Reservations { get; set; }
 }
