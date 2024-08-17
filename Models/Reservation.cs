4f2141c45bf3d0e6725cbf5324b4f89ab42ec644
Taylor Seager <taylorseager718@gmail.com>
2024-08-17 13:42:31 -0500

    updated reservation model

--- Models/Reservation.cs
+++ Models/Reservation.cs
@@ -9,4 +9,5 @@ public class Reservation
     public UserProfile UserProfile { get; set; }
     public DateTime CheckinDate { get; set; }
     public DateTime CheckoutDate { get; set; }
+    public int TotalNights => (CheckoutDate - CheckinDate).Days;
 }
