dc0f0fa7b238de4a2955919012c43f53ccbd2725
Taylor Seager <taylorseager718@gmail.com>
2024-08-17 10:31:32 -0500

    DbContext created, OnModelCreatting, model builder campsiteType and Campsite added

---
+++ CreekRiverDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CreekRiver.Models;
+
+public class CreekRiverDbContext : DbContext
+{
+
+    public DbSet<Reservation> Reservations { get; set; }
+    public DbSet<UserProfile> UserProfiles { get; set; }
+    public DbSet<Campsite> Campsites { get; set; }
+    public DbSet<CampsiteType> CampsiteTypes { get; set; }
+
+    public CreekRiverDbContext(DbContextOptions<CreekRiverDbContext> context) : base(context)
+    {
+
+    }
+
+protected override void OnModelCreating(ModelBuilder modelBuilder)
+{
+    // seed data with campsite types
+    modelBuilder.Entity<CampsiteType>().HasData(new CampsiteType[]
+    {
+        new CampsiteType {Id = 1, CampsiteTypeName = "Tent", FeePerNight = 15.99M, MaxReservationDays = 7},
+        new CampsiteType {Id = 2, CampsiteTypeName = "RV", FeePerNight = 26.50M, MaxReservationDays = 14},
+        new CampsiteType {Id = 3, CampsiteTypeName = "Primitive", FeePerNight = 10.00M, MaxReservationDays = 3},
+        new CampsiteType {Id = 4, CampsiteTypeName = "Hammock", FeePerNight = 12M, MaxReservationDays = 7}
+    });
+        modelBuilder.Entity<Campsite>().HasData(new Campsite[]
+    {
+    new Campsite {Id = 1, CampsiteTypeId = 1, Nickname = "Barred Owl", ImageUrl="https://tnstateparks.com/assets/images/content-images/campgrounds/249/colsp-area2-site73.jpg"},
+    });
+}
+}
+
+
+// DbContext represents database as .NET object
+// DbSet class from EF Core represents database entities, can write Linq queires (which are transformed into SQL query
